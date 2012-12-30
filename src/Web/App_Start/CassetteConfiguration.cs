0a0b54fe025b8d87783945b68d3b0ebabe8a6231
James McKay <code@jamesmckay.net>
2012-12-30 17:57:18 +0000

    Configure Cassette with a default stylesheet.

--- src/Web/App_Start/CassetteConfiguration.cs
+++ src/Web/App_Start/CassetteConfiguration.cs
@@ -17,17 +17,22 @@ namespace Dolstagis.Web.App_Start
             // This default configuration treats each file as a separate 'bundle'.
             // In production the content will be minified, but the files are not combined.
             // So you probably want to tweak these defaults!
+
+
             //bundles.AddPerIndividualFile<StylesheetBundle>("Content");
             //bundles.AddPerIndividualFile<ScriptBundle>("Scripts");
 
             // To combine files, try something like this instead:
             //   bundles.Add<StylesheetBundle>("Content");
             // In production mode, all of ~/Content will be combined into a single bundle.
-            
+
             // If you want a bundle per folder, try this:
             //   bundles.AddPerSubDirectory<ScriptBundle>("Scripts");
             // Each immediate sub-directory of ~/Scripts will be combined into its own bundle.
             // This is useful when there are lots of scripts for different areas of the website.
+
+            bundles.AddPerSubDirectory<ScriptBundle>("Scripts");
+            bundles.AddPerSubDirectory<StylesheetBundle>("Content");
         }
     }
 }
