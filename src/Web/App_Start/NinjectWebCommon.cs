f7822f97cd0dd494057e7cc7bf3484f74173cca1
James McKay <code@jamesmckay.net>
2013-01-02 10:13:50 +0000

    Sort usings in Dolstagis.Web.Common

--- src/Web/App_Start/NinjectWebCommon.cs
+++ src/Web/App_Start/NinjectWebCommon.cs
@@ -1,17 +1,15 @@
+using Dolstagis.Core.Data;
+using Microsoft.Web.Infrastructure.DynamicModuleHelper;
+using Ninject;
+using Ninject.Web.Common;
+using System;
+using System.Web;
+
 [assembly: WebActivator.PreApplicationStartMethod(typeof(Dolstagis.Web.App_Start.NinjectWebCommon), "Start")]
 [assembly: WebActivator.ApplicationShutdownMethodAttribute(typeof(Dolstagis.Web.App_Start.NinjectWebCommon), "Stop")]
 
 namespace Dolstagis.Web.App_Start
 {
-    using System;
-    using System.Web;
-
-    using Microsoft.Web.Infrastructure.DynamicModuleHelper;
-
-    using Ninject;
-    using Ninject.Web.Common;
-    using Dolstagis.Core.Data;
-
     public static class NinjectWebCommon 
     {
         private static readonly Bootstrapper bootstrapper = new Bootstrapper();
