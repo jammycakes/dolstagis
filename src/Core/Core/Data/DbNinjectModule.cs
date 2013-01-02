6c7659f4887e8c13e8f54374136d41f3c15acebe
James McKay <code@jamesmckay.net>
2013-01-02 02:26:15 +0000

    Add dependency injection for NHibernate ISession

---
+++ src/Core/Core/Data/DbNinjectModule.cs
@@ -0,0 +1,45 @@
+﻿using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using NHibernate;
+using Ninject.Modules;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+
+namespace Dolstagis.Core.Data
+{
+    public class DbNinjectModule : NinjectModule
+    {
+        private ISessionFactory sessionFactory;
+
+        public DbNinjectModule(string connectionString)
+        {
+            string[] keys = new string[] {
+                connectionString,
+                connectionString + "." + Environment.MachineName
+            };
+
+            var cs = keys.Select(x => ConfigurationManager.ConnectionStrings[x])
+                .Where(x => x != null)
+                .Select(x => x.ConnectionString)
+                .FirstOrDefault(x => x != null)
+                ?? connectionString;
+
+            this.sessionFactory = Fluently.Configure()
+                .Database(MsSqlConfiguration.MsSql2008
+                    .ConnectionString(cs)
+                    .FormatSql().DoNot.ShowSql()
+                )
+                .Mappings(x => x.FluentMappings.AddFromAssembly(this.GetType().Assembly))
+                .BuildSessionFactory();
+        }
+
+
+        public override void Load()
+        {
+            Rebind<ISession>().ToMethod(x => this.sessionFactory.OpenSession());
+        }
+    }
+}
--- src/Web/App_Start/NinjectWebCommon.cs
+++ src/Web/App_Start/NinjectWebCommon.cs
@@ -10,6 +10,7 @@ namespace Dolstagis.Web.App_Start
 
     using Ninject;
     using Ninject.Web.Common;
+    using Dolstagis.Core.Data;
 
     public static class NinjectWebCommon 
     {
@@ -53,6 +54,7 @@ namespace Dolstagis.Web.App_Start
         /// <param name="kernel">The kernel.</param>
         private static void RegisterServices(IKernel kernel)
         {
+            kernel.Load(new DbNinjectModule("Dolstagis"));
         }
     }
 }
