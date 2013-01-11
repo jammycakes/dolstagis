ac1ae5e07122b0562ddefbb3898571dc7f5e3fa4
James McKay <code@jamesmckay.net>
2013-01-11 19:17:10 +0000

    Add a local filespace object.

---
+++ src/Core/Core/IO/IFilespace.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Dolstagis.Core.IO
+{
+    /// <summary>
+    ///  Provides a mockable, injectable interface around a filespace.
+    ///  This may have multiple implementations including Local, HTTP and Resources.
+    /// </summary>
+    public interface IFilespace
+    {
+        /// <summary>
+        ///  Appends some text to an existing file.
+        /// </summary>
+        /// <param name="filename">
+        ///  The name of the file to append to.
+        /// </param>
+        /// <param name="contents">
+        ///  The text to append.
+        /// </param>
+
+        void Append(string filename, string contents);
+
+        /// <summary>
+        ///  Appends one or more lines to an existing file.
+        /// </summary>
+        /// <param name="filename">
+        ///  The name of the file to append to.
+        /// </param>
+        /// <param name="contents">
+        ///  The lines to append.
+        /// </param>
+
+        void AppendLines(string filename, params string[] lines);
+
+        /// <summary>
+        ///  Opens a text file for reading.
+        /// </summary>
+        /// <param name="filename">
+        ///  The name of the file to open.
+        /// </param>
+        /// <returns>
+        ///  A <see cref="TextReader"/> instance.
+        /// </returns>
+
+        TextReader OpenReader(string filename);
+
+        /// <summary>
+        ///  Opens a text file for writing.
+        /// </summary>
+        /// <param name="filename">
+        ///  The name of the file to open.
+        /// </param>
+        /// <param name="append">
+        ///  true to append to the file, otherwise false.
+        /// </param>
+        /// <returns>
+        ///  A <see cref="TextWriter"/> instance.
+        /// </returns>
+
+        TextWriter OpenWriter(string filename, bool append = false);
+
+        /// <summary>
+        ///  Reads the entire contents of a file to a string.
+        /// </summary>
+        /// <param name="filename">
+        ///  The name of the file to read.
+        /// </param>
+        /// <returns>
+        ///  A string containing the contents of the file.
+        /// </returns>
+
+        string Read(string filename);
+
+        /// <summary>
+        ///  Writes a string to a file.
+        /// </summary>
+        /// <param name="filename">
+        ///  The name of the file to write to.
+        /// </param>
+        /// <param name="contents">
+        ///  The contents of the file to write.
+        /// </param>
+        
+        void Write(string filename, string contents);
+
+        /// <summary>
+        ///  Gets a value indicating whether the filespace is read-only or writable.
+        /// </summary>
+
+        bool ReadOnly { get; }
+    }
+}
---
+++ src/Core/Core/IO/LocalFilespace.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Dolstagis.Core.IO
+{
+    /// <summary>
+    ///  Represents a filespace in the local filesystem.
+    /// </summary>
+
+    public class LocalFilespace : IFilespace
+    {
+        public string Root { get; private set; }
+
+        public Encoding Encoding { get; private set; }
+
+        /// <summary>
+        ///  Gets a value indicating whether the filespace is read-only or writable.
+        /// </summary>
+
+        public bool ReadOnly { get { return false; } }
+
+        /// <summary>
+        ///  Creates a new instance of the <see cref="LocalFilespace"/> class,
+        ///  rooted at a specified directory.
+        /// </summary>
+        /// <param name="root">
+        ///  The root directory of the filespace.
+        /// </param>
+        /// <param name="encoding">
+        ///  The default encoding to use to read and write to the files.
+        ///  If none is specified, UTF-8 is assumed.
+        /// </param>
+
+        public LocalFilespace(string root, Encoding encoding = null)
+        {
+            this.Root = new DirectoryInfo(root).FullName;
+            if (!this.Root.EndsWith(Path.DirectorySeparatorChar.ToString()))
+                this.Root += Path.DirectorySeparatorChar;
+            this.Encoding = encoding ?? Encoding.UTF8;
+        }
+
+        /// <summary>
+        ///  Converts a relative path within the filespace to an absolute one.
+        /// </summary>
+        /// <param name="filename">
+        ///  The name of the file.
+        /// </param>
+        /// <returns>
+        ///  The absolute path to the file.
+        /// </returns>
+        /// <exception cref="ArgumentException">
+        ///  The filename specifies a path to a file outside this filespace.
+        /// </exception>
+
+        public string GetAbsolutePath(string filename)
+        {
+            string path = Path.Combine(this.Root, filename);
+            if (!path.StartsWith(this.Root, StringComparison.InvariantCultureIgnoreCase))
+                throw new ArgumentException("Specified file is not within the filespace.", "filename");
+            return new FileInfo(path).FullName;
+        }
+
+        /// <summary>
+        ///  Appends some text to an existing file.
+        /// </summary>
+        /// <param name="filename">
+        ///  The name of the file to append to.
+        /// </param>
+        /// <param name="contents">
+        ///  The text to append.
+        /// </param>
+        /// <exception cref="ArgumentException">
+        ///  The filename specifies a path to a file outside this filespace.
+        /// </exception>
+
+        public void Append(string filename, string contents)
+        {
+            File.AppendAllText(GetAbsolutePath(filename), contents, this.Encoding);
+        }
+
+        /// <summary>
+        ///  Appends one or more lines to an existing file.
+        /// </summary>
+        /// <param name="filename">
+        ///  The name of the file to append to.
+        /// </param>
+        /// <param name="contents">
+        ///  The lines to append.
+        /// </param>
+        /// <exception cref="ArgumentException">
+        ///  The filename specifies a path to a file outside this filespace.
+        /// </exception>
+
+        public void AppendLines(string filename, params string[] lines)
+        {
+            File.AppendAllLines(GetAbsolutePath(filename), lines, this.Encoding);
+        }
+
+        /// <summary>
+        ///  Opens a text file for reading.
+        /// </summary>
+        /// <param name="filename">
+        ///  The name of the file to open.
+        /// </param>
+        /// <returns>
+        ///  A <see cref="TextReader"/> instance.
+        /// </returns>
+        /// <exception cref="ArgumentException">
+        ///  The filename specifies a path to a file outside this filespace.
+        /// </exception>
+
+        public TextReader OpenReader(string filename)
+        {
+            return new StreamReader(GetAbsolutePath(filename), this.Encoding);
+        }
+
+        /// <summary>
+        ///  Opens a text file for writing.
+        /// </summary>
+        /// <param name="filename">
+        ///  The name of the file to open.
+        /// </param>
+        /// <param name="append">
+        ///  true to append to the file, otherwise false.
+        /// </param>
+        /// <returns>
+        ///  A <see cref="TextWriter"/> instance.
+        /// </returns>
+        /// <exception cref="ArgumentException">
+        ///  The filename specifies a path to a file outside this filespace.
+        /// </exception>
+
+        public TextWriter OpenWriter(string filename, bool append = false)
+        {
+            return new StreamWriter(GetAbsolutePath(filename), append, this.Encoding);
+        }
+
+        /// <summary>
+        ///  Reads the entire contents of a file to a string.
+        /// </summary>
+        /// <param name="filename">
+        ///  The name of the file to read.
+        /// </param>
+        /// <returns>
+        ///  A string containing the contents of the file.
+        /// </returns>
+        /// <exception cref="ArgumentException">
+        ///  The filename specifies a path to a file outside this filespace.
+        /// </exception>
+
+        public string Read(string filename)
+        {
+            return File.ReadAllText(GetAbsolutePath(filename), this.Encoding);
+        }
+
+        /// <summary>
+        ///  Writes a string to a file.
+        /// </summary>
+        /// <param name="filename">
+        ///  The name of the file to write to.
+        /// </param>
+        /// <param name="contents">
+        ///  The contents of the file to write.
+        /// </param>
+        /// <exception cref="ArgumentException">
+        ///  The filename specifies a path to a file outside this filespace.
+        /// </exception>
+
+        public void Write(string filename, string contents)
+        {
+            File.WriteAllText(GetAbsolutePath(filename), contents, this.Encoding);
+        }
+    }
+}
