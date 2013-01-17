eea1ad2970414b2beb0c2d1eeeda2baf1622c56a
James McKay <code@jamesmckay.net>
2013-01-17 08:04:36 +0000

    Add some pre/post fixture/test methods for callers to override.
    These will run deterministically, ie after setup or before teardown functionality
    in the base classes has been run.
    
    TODO: check with NUnit spec to see if this happens anyway...

--- src/Tests/NHibernateFixtureBase.cs
+++ src/Tests/NHibernateFixtureBase.cs
@@ -14,11 +14,81 @@ namespace Dolstagis.Tests
 {
     public class NHibernateFixtureBase
     {
+        /// <summary>
+        ///  Service locator.
+        /// </summary>
+
         protected IKernel Kernel { get; private set; }
+
+        /// <summary>
+        ///  The NHibernate session.
+        /// </summary>
 
         protected ISession Session { get; private set; }
 
+        /// <summary>
+        ///  The database connection.
+        /// </summary>
+
         protected IDbConnection Connection { get; private set; }
+
+        /// <summary>
+        ///  Called before the tests in the fixture are run.
+        /// </summary>
+        /// <remarks>
+        ///  Implementors should override this method rather than create a new one
+        ///  with the [TestFixtureSetUp] attribute, otherwise the new methods are
+        ///  not guaranteed to run after the test mappings and database connection
+        ///  have been initialised.
+        /// </remarks>
+
+        protected virtual void BeforeFixture()
+        {
+        }
+
+        /// <summary>
+        ///  Called after the tests in the fixture have been run.
+        /// </summary>
+        /// <remarks>
+        ///  Implementors should override this method rather than create a new one
+        ///  with the [TestFixtureTearDown] attribute, otherwise the new methods are
+        ///  not guaranteed to run before the test database connection and services
+        ///  have been disposed.
+        /// </remarks>
+
+        protected virtual void AfterFixture()
+        {
+        }
+
+        /// <summary>
+        ///  Called immediately before each test in the fixture is run.
+        /// </summary>
+        /// <remarks>
+        ///  Implementors should override this method rather than create a new one
+        ///  with the [SetUp] attribute, otherwise the new methods are not guaranteed
+        ///  to run after the test session has been cleared.
+        /// </remarks>
+
+        protected virtual void BeforeTest()
+        {
+        }
+
+        /// <summary>
+        ///  Called immediately after each test in the fixture is run.
+        /// </summary>
+        /// <remarks>
+        ///  Implementors should override this method rather than create a new one
+        ///  with the [TearDown] attribute, otherwise the new methods are not guaranteed
+        ///  to run before the test session has been cleared.
+        /// </remarks>
+
+        protected virtual void AfterTest()
+        {
+        }
+
+        /// <summary>
+        ///  Sets up the database connection, mappings and Ninject bindings.
+        /// </summary>
 
         [TestFixtureSetUp]
         public void CreateSession()
@@ -32,24 +102,32 @@ namespace Dolstagis.Tests
             var schema = new SchemaExport(Kernel.Get<NHibernate.Cfg.Configuration>());
             schema.Execute(true, true, false, this.Session.Connection, null);
             this.Connection = this.Session.Connection;
+            this.BeforeFixture();
         }
+
+        /// <summary>
+        ///  Disposes of the test database connection.
+        /// </summary>
 
         [TestFixtureTearDown]
         public virtual void DisposeSession()
         {
+            this.AfterFixture();
             this.Session.Close();
-            Kernel.Dispose();
+            this.Kernel.Dispose();
         }
 
         [SetUp]
-        public virtual void BeforeTest()
+        public virtual void PrepareTest()
         {
             this.Session.Clear();
+            this.BeforeTest();
         }
 
         [TearDown]
-        public virtual void AfterTest()
+        public virtual void CompleteTest()
         {
+            this.AfterTest();
             this.Session.Clear();
         }
     }
