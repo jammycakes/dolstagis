df870eb23e501d247e752adebe7f3c6afb7cfc7c
James McKay <code@jamesmckay.net>
2013-01-29 07:39:25 +0000

    Add base class for web.config settings.

---
+++ src/Core/Core/AppConfigSettingsBase.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Dolstagis.Core
+{
+    /// <summary>
+    ///  Provides a base class for reading settings from the appSettings section
+    ///  of the web.config file.
+    /// </summary>
+    /// <remarks>
+    ///  <para>
+    ///   To add a new configuration setting, simply create a new property in the
+    ///   derived class. It will automatically be configured by reflection if it is
+    ///   present in the base class. Properties may have private setters if desired.
+    ///  </para>
+    ///  <para>
+    ///   By default, appSettings should be prefixed by the namespace of the class
+    ///   in which they are defined. See the test cases for an example.
+    ///  </para>
+    /// </remarks>
+
+    public class AppConfigSettingsBase
+    {
+        private string GetBaseName()
+        {
+            return this.GetType().Namespace + ".";
+        }
+
+        public AppConfigSettingsBase()
+        {
+            string baseName = GetBaseName();
+
+            foreach (var prop in this.GetType().GetProperties()) {
+                if (prop.CanWrite && !prop.GetIndexParameters().Any()) {
+                    string key = baseName + prop.Name;
+                    string value = ConfigurationManager.AppSettings[key];
+                    if (value != null) {
+                        SetValue(prop, value);
+                    }
+                }
+            }
+        }
+
+        private void SetValue(PropertyInfo prop, string value)
+        {
+            if (prop.PropertyType.IsAssignableFrom(typeof(string))) {
+                prop.SetValue(this, value, null);
+            }
+            else if (prop.PropertyType.IsAssignableFrom(typeof(Int32))) {
+                prop.SetValue(this, Int32.Parse(value), null);
+            }
+            else if (prop.PropertyType.IsAssignableFrom(typeof(Boolean))) {
+                prop.SetValue(this, Boolean.Parse(value), null);
+            }
+            else if (prop.PropertyType.IsEnum) {
+                prop.SetValue(this, Enum.Parse(prop.PropertyType, value, true), null);
+            }
+            else if (prop.PropertyType.IsAssignableFrom(typeof(DateTime))) {
+                prop.SetValue(this, DateTime.Parse(value), null);
+            }
+        }
+    }
+}
---
+++ src/Tests/Core/AppConfigSettingsFixture.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Dolstagis.Tests.Core
+{
+    [TestFixture]
+    public class AppConfigSettingsFixture
+    {
+        private AppSettingsTestData testData;
+
+        [TestFixtureSetUp]
+        public void CreateTestData()
+        {
+            this.testData = new AppSettingsTestData();
+        }
+
+        [Test]
+        public void CanParseIntValue()
+        {
+            Assert.AreEqual(1, testData.IntValue);
+        }
+
+        [Test]
+        public void CanParseStringValue()
+        {
+            Assert.AreEqual("Dolstagis", testData.StringValue);
+        }
+
+        [Test]
+        public void CanParseBoolValue()
+        {
+            Assert.IsTrue(testData.BoolValue);
+        }
+
+        [Test]
+        public void CanParseDateTimeValue()
+        {
+            Assert.AreEqual(new DateTime(2013, 1, 29, 7, 34, 0), testData.DateTimeValue);
+        }
+
+        [Test]
+        public void CanParseEnumValue()
+        {
+            Assert.AreEqual(DateTimeKind.Local, testData.EnumValue);
+        }
+    }
+}
