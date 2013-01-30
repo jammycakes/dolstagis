469a50520f55bfd5869046b31f6a66067f5704d5
James McKay <code@jamesmckay.net>
2013-01-30 07:30:47 +0000

    Add some comments to the UserToken class.

--- src/Core/Accounts/UserToken.cs
+++ src/Core/Accounts/UserToken.cs
@@ -6,18 +6,47 @@ using System.Text;
 
 namespace Dolstagis.Accounts
 {
+    /// <summary>
+    ///  Represents a token that is e-mailed to the user to perform a specific action.
+    /// </summary>
+
     public class UserToken
     {
+        /// <summary>
+        ///  The token identifier.
+        /// </summary>
+
         public virtual Guid Token { get; protected set; }
+
+        /// <summary>
+        ///  The user to whom this token was sent.
+        /// </summary>
 
         public virtual User User { get; protected set; }
 
+        /// <summary>
+        ///  The controller action to be carried out when this token is used
+        ///  on the website.
+        /// </summary>
+
         public virtual string Action { get; protected set; }
+
+        /// <summary>
+        ///  The UTC date and time that the token expires.
+        /// </summary>
 
         public virtual DateTime Expires { get; protected set; }
 
         protected UserToken()
         { }
+
+        /// <summary>
+        ///  Creates a new instance of the <see cref="UserToken"/> instance
+        ///  for a specific user, action and expiry date.
+        /// </summary>
+        /// <param name="user"></param>
+        /// <param name="action"></param>
+        /// <param name="expires"></param>
 
         public UserToken(User user, string action, DateTime expires)
         {
