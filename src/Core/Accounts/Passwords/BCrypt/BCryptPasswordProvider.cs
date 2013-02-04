2acf9cf2c6b67d530b2617877151bc6f684cc0d6
James McKay <code@jamesmckay.net>
2013-02-04 07:56:10 +0000

    Return correct values from bcrypt for suboptimal and unrecognised hashes.

--- src/Core/Accounts/Passwords/BCrypt/BCryptPasswordProvider.cs
+++ src/Core/Accounts/Passwords/BCrypt/BCryptPasswordProvider.cs
@@ -1,4 +1,5 @@
 ﻿using bc = BCrypt.Net;
+using NLog;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,6 +13,8 @@ namespace Dolstagis.Accounts.Passwords.BCrypt
 
     public class BCryptPasswordProvider : IPasswordProvider
     {
+        Logger log = LogManager.GetCurrentClassLogger();
+
         public IBCryptSettings Settings { get; private set; }
 
         public BCryptPasswordProvider(IBCryptSettings settings)
@@ -26,8 +29,33 @@ namespace Dolstagis.Accounts.Passwords.BCrypt
 
         public PasswordResult Verify(string password, string hash)
         {
-            if (bc.BCrypt.Verify(password, hash)) {
-                return PasswordResult.Correct;
+            string[] parts = hash.Split('$');
+
+            // "$2a$10$x3A4kgKFKj3ojOhqfm2NgOq.4R5lQl/kWsOAxi7IfQs/VF.YmUTN6"
+
+            if (parts.Length < 4) return PasswordResult.Unrecognised;
+            if (parts[0] != String.Empty) return PasswordResult.Unrecognised;
+            if (parts[1] != "2a") return PasswordResult.Unrecognised;
+            int workFactor;
+            if (!Int32.TryParse(parts[2], out workFactor)) return PasswordResult.Unrecognised;
+
+            bool isCorrect;
+
+            try {
+                isCorrect = bc.BCrypt.Verify(password, hash);
+            }
+            catch (bc.SaltParseException) {
+                log.Warn(() => "Invalid salt: " + hash);
+                return PasswordResult.Unrecognised;
+            }
+
+            if (isCorrect) {
+                if (workFactor < Settings.WorkFactor) {
+                    return PasswordResult.CorrectButInsecure;
+                }
+                else {
+                    return PasswordResult.Correct;
+                }
             }
             else {
                 return PasswordResult.Incorrect;
---
+++ src/Tests/Accounts/Passwords/BCrypt/BCryptPasswordProviderFixture.cs
@@ -0,0 +1,47 @@
+﻿using Dolstagis.Accounts.Passwords;
+using Dolstagis.Accounts.Passwords.BCrypt;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace Dolstagis.Tests.Accounts.Passwords.BCrypt
+{
+    [TestFixture]
+    public class BCryptPasswordProviderFixture
+    {
+        private BCryptPasswordProvider GetProvider(int workFactor)
+        {
+            var settings = new Mock<IBCryptSettings>();
+            settings.Setup(x => x.WorkFactor).Returns(workFactor);
+            return new BCryptPasswordProvider(settings.Object);
+        }
+
+        [Test]
+        public void CanHashAndVerifyPassword()
+        {
+            string password = "Passw0rD1";
+            int workFactor = 5;
+
+            var provider = GetProvider(workFactor);
+            var hash = provider.ComputeHash(password);
+            var testResult = provider.Verify(password, hash);
+            Assert.AreEqual(PasswordResult.Correct, testResult);
+        }
+
+        [Test]
+        public void CanHashAndVerifyInsecurePassword()
+        {
+            string password = "Passw0rd1";
+            int oldWorkFactor = 5;
+            int newWorkFactor = 10;
+
+            var hash = GetProvider(oldWorkFactor).ComputeHash(password);
+            var testResult = GetProvider(newWorkFactor).Verify(password, hash);
+            Assert.AreEqual(PasswordResult.CorrectButInsecure, testResult);
+        }
+    }
+}
