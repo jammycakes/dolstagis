3b23cd3073d4a3a93f09cd653bd97fb0a2a7c226
James McKay <code@jamesmckay.net>
2013-02-21 07:55:05 +0000

    Create a set of conventions on where to put the CSS declarations.
    --HG--
    rename : src/Web/Content/colours.less => src/Web/Content/core/colours.less
    rename : src/Web/Content/core.less => src/Web/Content/core/core.less
    rename : src/Web/Content/site.less => src/Web/Content/layout.less
    rename : src/Web/Content/flash.less => src/Web/Content/layout/flash.less
    rename : src/Web/Content/topbanner.less => src/Web/Content/layout/topbanner.less

--- src/Web/App_Start/CassetteConfiguration.cs
+++ src/Web/App_Start/CassetteConfiguration.cs
@@ -11,28 +11,19 @@ namespace Dolstagis.Web.App_Start
     {
         public void Configure(BundleCollection bundles)
         {
-            // TODO: Configure your bundles here...
-            // Please read http://getcassette.net/documentation/configuration
-
-            // This default configuration treats each file as a separate 'bundle'.
-            // In production the content will be minified, but the files are not combined.
-            // So you probably want to tweak these defaults!
-
-
-            //bundles.AddPerIndividualFile<StylesheetBundle>("Content");
-            //bundles.AddPerIndividualFile<ScriptBundle>("Scripts");
-
-            // To combine files, try something like this instead:
-            //   bundles.Add<StylesheetBundle>("Content");
-            // In production mode, all of ~/Content will be combined into a single bundle.
-
-            // If you want a bundle per folder, try this:
-            //   bundles.AddPerSubDirectory<ScriptBundle>("Scripts");
-            // Each immediate sub-directory of ~/Scripts will be combined into its own bundle.
-            // This is useful when there are lots of scripts for different areas of the website.
-
             bundles.AddPerSubDirectory<ScriptBundle>("Scripts");
-            bundles.AddPerSubDirectory<StylesheetBundle>("Content");
+
+            /*
+             * Stylesheets are configured per file. This is because Less CSS
+             * files can specify their own imports, which are incorporated into
+             * the output for that file and are not rendered independently.
+             * This means that to have one stylesheet (and therefore, one
+             * bundle) per layout page, we can reference a single file in the
+             * root of the stylesheets folder, and still modularise our
+             * dependencies.
+             */
+
+            bundles.AddPerIndividualFile<StylesheetBundle>("Content");
         }
     }
 }
---
+++ src/Web/Content/readme.txt
@@ -0,0 +1,16 @@
+﻿CSS stylesheet folder layout
+============================
+
+All stylesheets in this folder are compiled using Less CSS.
+
+Only stylesheets in the top level folder are loaded directly into the page.
+Stylesheets in subfolders are loaded in to the main stylesheet using an @import
+directive. This way, we can keep our stylesheets modular.
+
+You should have at most one stylesheet for each layout page. The top level
+stylesheet should have a name that mirrors the name of the layout page itself
+(e.g. _Layout.cshtml -> layout.less) and any imports into that stylesheet should
+be placed in a subfolder with a matching name, e.g. layout.
+
+Mixins, constants and other Less declarations that do not directly translate to
+markup should be placed in the "core" folder.
