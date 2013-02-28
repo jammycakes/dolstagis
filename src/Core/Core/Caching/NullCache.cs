0c9ba1177b81b2e9b8084f76a6b8ec9fb36e24ec
James McKay <code@jamesmckay.net>
2013-02-28 17:45:50 +0000

    Add a NullCache implementation, which doesn't do anything.

---
+++ src/Core/Core/Caching/NullCache.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dolstagis.Core.Caching
+{
+    public class NullCache : ICache
+    {
+        public object Add(string key, object value, System.Web.Caching.CacheDependency dependencies, DateTime absoluteExpiration, TimeSpan slidingExpiration, System.Web.Caching.CacheItemPriority priority, System.Web.Caching.CacheItemRemovedCallback onRemoveCallback)
+        {
+            return null;
+        }
+
+        public int Count
+        {
+            get { return 0; }
+        }
+
+        public object Get(string key)
+        {
+            return null;
+        }
+
+        public void Insert(string key, object value)
+        { }
+
+        public void Insert(string key, object value, System.Web.Caching.CacheDependency dependencies)
+        { }
+
+        public void Insert(string key, object value, System.Web.Caching.CacheDependency dependencies, DateTime absoluteExpiration, TimeSpan slidingExpiration)
+        { }
+
+        public void Insert(string key, object value, System.Web.Caching.CacheDependency dependencies, DateTime absoluteExpiration, TimeSpan slidingExpiration, System.Web.Caching.CacheItemPriority priority, System.Web.Caching.CacheItemRemovedCallback onRemoveCallback)
+        { }
+
+        public void Insert(string key, object value, System.Web.Caching.CacheDependency dependencies, DateTime absoluteExpiration, TimeSpan slidingExpiration, System.Web.Caching.CacheItemUpdateCallback onUpdateCallback)
+        { }
+
+        public object Remove(string key)
+        {
+            return null;
+        }
+
+        public object this[string key]
+        {
+            get
+            {
+                return null;
+            }
+            set
+            { }
+        }
+    }
+}
