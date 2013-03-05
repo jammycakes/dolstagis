6a7a4dbca0549e1a6f248d5e463e90523364708a
James McKay <code@jamesmckay.net>
2013-03-05 05:09:46 +0000

    IP address should not be part of the primary key for user sessions.

--- src/DataMigrations/0001/CreateUserSessionsTable.cs
+++ src/DataMigrations/0001/CreateUserSessionsTable.cs
@@ -18,7 +18,7 @@ namespace Dolstagis.DataMigrations._0001
                     .ForeignKey("Users", "UserID").OnUpdate(Rule.Cascade).OnDelete(Rule.Cascade)
                 .WithColumn("DateCreated").AsDateTime().NotNullable()
                 .WithColumn("DateLastAccessed").AsDateTime().NotNullable()
-                .WithColumn("IPAddress").AsAnsiString(50).PrimaryKey().NotNullable()
+                .WithColumn("IPAddress").AsAnsiString(50).NotNullable()
                 .WithColumn("UserAgentID").AsInt32().NotNullable()
                     .ForeignKey("UserAgents", "UserAgentID").OnUpdate(Rule.None).OnDelete(Rule.None);
         }
