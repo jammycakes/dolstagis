e09ee9f5e7464b715b84fa9bdb70c8822bd4b0c1
James McKay <code@jamesmckay.net>
2013-03-11 08:34:22 +0000

    Add delete with fadeout effect.

---
+++ src/Web/Scripts/effects.js
@@ -0,0 +1,22 @@
+﻿// @reference jquery-1.8.3.js
+
+(function ($, undefined) {
+  "use strict";
+
+  var fadeStart = '#ff9933';
+  var duration = 500;
+
+  $.fn.fadeDelete = function (callback) {
+    var self = this;
+    return this.css('background-color', fadeStart)
+      .animate({
+        opacity: 0
+      }, duration, function () {
+        self.remove();
+        if (typeof(callback) === 'function') {
+          callback();
+        }
+      });
+  };
+
+})(jQuery);
