c042c7c1f90ef0c327fe04f85298153275a7bcce
James McKay <code@jamesmckay.net>
2013-03-11 08:35:03 +0000

    Delete a session row from the DOM, with fadeout effect.
    Still todo: AJAX postback to server side.

---
+++ src/Web/Scripts/user/sessions.js
@@ -0,0 +1,14 @@
+﻿// @reference ../effects.js
+// @reference ../jquery-1.8.3.js
+
+(function ($, undefined) {
+  "use strict";
+
+  $(function () {
+    $('#user-sessions').on('click', '.delete', function (e) {
+      $(this).parents('tr').first().fadeDelete();
+      e.preventDefault();
+    });
+  });
+
+})(jQuery);
