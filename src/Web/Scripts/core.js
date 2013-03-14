b2a08c82691d4dc2601fbeea953d973d2c8565e3
James McKay <code@jamesmckay.net>
2013-03-14 08:19:50 +0000

    Implement client-side message flash

---
+++ src/Web/Scripts/core.js
@@ -0,0 +1,9 @@
+﻿// @reference jquery-1.8.3.js
+
+var dolstagis = window.dolstagis = {
+  core: {
+  }
+};
+
+var console = window.console = window.console || {};
+console.log = console.log || function () { };
--- src/Web/Scripts/effects.js
+++ src/Web/Scripts/effects.js
@@ -1,4 +1,5 @@
 ﻿// @reference jquery-1.8.3.js
+// @reference core.js
 
 (function ($, undefined) {
   "use strict";
@@ -17,6 +18,32 @@
           callback();
         }
       });
+  };
+
+  dolstagis.ui = {
+    flash: function (message, level) {
+
+      var container = document.getElementById('flash');
+      if (!container) {
+        container = document.createElement('ul');
+        container.id = 'flash';
+        var anchor = document.getElementById('topbanner');
+        anchor.parentNode.insertBefore(container, anchor.nextSibling)
+      }
+
+      var $anchor = $(container).children('.' + level).last();
+      var newElement = document.createElement('li');
+      newElement.className = level;
+      newElement.style.display = "none";
+      newElement.innerHTML = message;
+      if ($anchor.length) {
+        $anchor.after(newElement);
+      }
+      else {
+        container.appendChild(newElement);
+      }
+      $(newElement).slideDown();
+    }
   };
 
 })(jQuery);
