e629b9d3e239f1029bd1067473500d4e4ce6fb2b
James McKay <code@jamesmckay.net>
2013-03-19 17:39:40 +0000

    Add and use define function in dolstagis.core for convenient namespacing syntax.

--- src/Web/Scripts/core.js
+++ src/Web/Scripts/core.js
@@ -1,7 +1,17 @@
 ﻿// @reference jquery-1.8.3.js
 
+
 var dolstagis = window.dolstagis = {
-  core: {
+
+  define: function define(ns, target) {
+    "use strict";
+
+    var parts = ns.split('.');
+    var obj = dolstagis;
+    while (parts.length > 0) {
+      var part = parts.shift();
+      obj = obj[part] = obj[part] || parts.length ? { } : target;
+    }
   }
 };
 
--- src/Web/Scripts/effects.js
+++ src/Web/Scripts/effects.js
@@ -20,31 +20,29 @@
       });
   };
 
-  dolstagis.ui = {
-    flash: function (message, level) {
-
-      var container = document.getElementById('flash');
-      if (!container) {
-        container = document.createElement('ul');
-        container.id = 'flash';
-        var anchor = document.getElementById('topbanner');
-        anchor.parentNode.insertBefore(container, anchor.nextSibling)
-      }
-
-      var $anchor = $(container).children('.' + level).last();
-      var newElement = document.createElement('li');
-      newElement.className = level;
-      newElement.style.display = "none";
-      newElement.innerHTML = message;
-      if ($anchor.length) {
-        $anchor.after(newElement);
-      }
-      else {
-        container.appendChild(newElement);
-      }
-      $(newElement).slideDown();
+  dolstagis.define('ui.flash', function (message, level) {
+    var container = document.getElementById('flash');
+    if (!container) {
+      container = document.createElement('ul');
+      container.id = 'flash';
+      var anchor = document.getElementById('topbanner');
+      anchor.parentNode.insertBefore(container, anchor.nextSibling)
     }
-  };
+
+    var $anchor = $(container).children('.' + level).last();
+    var newElement = document.createElement('li');
+    newElement.className = level;
+    newElement.style.display = "none";
+    newElement.innerHTML = message;
+    if ($anchor.length) {
+      $anchor.after(newElement);
+    }
+    else {
+      container.appendChild(newElement);
+    }
+    $(newElement).slideDown();
+  });
+
 
   $(function () {
     $(document).ajaxError(function (event, jqXHR, ajaxSettings, thrownError) {
