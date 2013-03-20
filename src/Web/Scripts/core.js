8dae98b9255148069a4e354a99a61f4db0acbf66
James McKay <code@jamesmckay.net>
2013-03-20 07:30:01 +0000

    Tidy up core.js: put it all in an IIFE and move console fallback to top.

--- src/Web/Scripts/core.js
+++ src/Web/Scripts/core.js
@@ -1,18 +1,19 @@
 ﻿// @reference jquery-1.8.3.js
 
-var dolstagis = window.dolstagis = {
+(function () {
+  "use strict";
 
-  define: function define(ns, target) {
-    "use strict";
+  var console = window.console = window.console || {};
+  console.log = console.log || function () { };
 
-    var parts = ns.split('.');
-    var obj = dolstagis;
-    while (parts.length > 0) {
-      var part = parts.shift();
-      obj = obj[part] = obj[part] || parts.length ? { } : target;
+  var dolstagis = window.dolstagis = {
+    define: function define(ns, target) {
+      var parts = ns.split('.');
+      var obj = dolstagis;
+      while (parts.length > 0) {
+        var part = parts.shift();
+        obj = obj[part] = obj[part] || parts.length ? {} : target;
+      }
     }
-  }
-};
-
-var console = window.console = window.console || {};
-console.log = console.log || function () { };
+  };
+}).call(this);
