61be638a142b1104f4f104e88e53e055d6ec29c3
James McKay <code@jamesmckay.net>
2013-04-15 18:10:36 +0100

    Add admin area, with user controller and default view.

---
+++ src/Web/Areas/Admin/AdminAreaRegistration.cs
@@ -0,0 +1,36 @@
+﻿using System.Web.Http;
+using System.Web.Mvc;
+using Dolstagis.Web.Infrastructure;
+
+namespace Dolstagis.Web.Areas.Admin
+{
+    public class AdminAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "Admin";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapHttpRoute(
+                name: "Admin_ajax",
+                routeTemplate: "admin/ajax/{action}/{id}",
+                defaults: new {
+                    controller = "AdminAjax",
+                    id = RouteParameter.Optional
+                }
+            );
+
+
+            context.MapRoute(
+                "Admin_default",
+                "admin/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
