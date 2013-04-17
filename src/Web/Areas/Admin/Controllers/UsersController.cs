8c89f2e87a85908b8a9788d14d7e3d3c41684fb2
James McKay <code@jamesmckay.net>
2013-04-17 17:47:35 +0100

    List users.

--- src/Web/Areas/Admin/Controllers/UsersController.cs
+++ src/Web/Areas/Admin/Controllers/UsersController.cs
@@ -3,18 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Dolstagis.Accounts;
 
 namespace Dolstagis.Web.Areas.Admin.Controllers
 {
     [Authorize(Roles="admin")]
     public class UsersController : Controller
     {
+        private UserManager userManager;
+
+        public UsersController(UserManager userManager)
+        {
+            this.userManager = userManager;
+        }
+
         //
-        // GET: /Admin/Admin/
+        // GET: /Admin/Users/
 
         public ActionResult Index()
         {
-            return View();
+            var users = userManager.GetAllUsers();
+            return View(users);
         }
     }
 }
