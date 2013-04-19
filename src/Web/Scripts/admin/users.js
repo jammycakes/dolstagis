1d09412d76e088c0d650ef02d6bf3b34353bb3b8
James McKay <code@jamesmckay.net>
2013-04-19 17:59:44 +0100

    Form to allocate invitations, and as yet non-functional JavaScript for it

---
+++ src/Web/Scripts/admin/users.js
@@ -0,0 +1,31 @@
+﻿// @reference ../jquery-1.8.3.js
+// @reference ../core.js
+// @reference ../ui.js
+
+(function ($, undefined) {
+  "use strict";
+
+  function allocateInvitations(e) {
+    var ids = $('input[name="select"]:checked')
+      .map(function (ix, el) { return $(el).val(); })
+      .toArray();
+    var count = parseInt($('#invitations').val(), 10);
+
+    $.post(
+      '/admin/ajax/AllocateInvitations/1',
+      { ids: ids, count: count },
+      function (data) {
+        console.log(data);
+      }
+    );
+
+
+
+
+    e.preventDefault();
+  }
+
+  $(function () {
+    $('#ctlAllocateInvitations').on('click', allocateInvitations);
+  });
+})(jQuery);
