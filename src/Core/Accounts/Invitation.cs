b95ce98789b976e9730612ac7b635e339b0c8141
James McKay <code@jamesmckay.net>
2013-05-01 17:44:10 +0100

    Invitation state.

--- src/Core/Accounts/Invitation.cs
+++ src/Core/Accounts/Invitation.cs
@@ -35,5 +35,52 @@ namespace Dolstagis.Accounts
         public Invitation()
         {
         }
+
+
+        public virtual InvitationState State
+        {
+            get
+            {
+                /*
+                 * An invitation has been accepted if it has an invitee user account.
+                 */
+                if (this.Invitee != null)
+                    return InvitationState.Accepted;
+
+                /*
+                 * An invitation has been orphaned if it has neither an inviting user
+                 * nor an invitee e-mail address
+                 */
+
+                if (String.IsNullOrEmpty(this.InviteeEmail) && this.InvitingUser == null)
+                    return InvitationState.Orphaned;
+
+                /*
+                 * An invitation has been sent if it has a send date.
+                 */
+                if (DateSent.HasValue)
+                    return InvitationState.Sent;
+
+                /*
+                 * An invitation has been requested if it has an invitee e-mail
+                 * but has not been sent.
+                 */
+                if (!String.IsNullOrEmpty(InviteeEmail))
+                    return InvitationState.Requested;
+
+                /*
+                 * An invitation has been allocated if it has an inviting user
+                 * but no invitee e-mail. If we get this far, this should always
+                 * be true.
+                 */
+                if (InvitingUser != null)
+                    return InvitationState.Allocated;
+
+                /*
+                 * This shouldn't ever run, but if it does, it will be orphaned.
+                 */
+                return InvitationState.Orphaned;
+            }
+        }
     }
 }
---
+++ src/Core/Accounts/InvitationState.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dolstagis.Accounts
+{
+    [Serializable]
+    public enum InvitationState
+    {
+        /// <summary>
+        ///  The invitation has been requested by a prospective invitee.
+        /// </summary>
+        Requested,
+
+        /// <summary>
+        ///  The invitation has been allocated to a registered user to send to a friend.
+        /// </summary>
+        Allocated,
+
+        /// <summary>
+        ///  The invitation has been sent to the invitee.
+        /// </summary>
+        Sent,
+
+        /// <summary>
+        ///  The invitation has been accepted by the invitee.
+        /// </summary>
+        Accepted,
+
+        /// <summary>
+        ///  The invitation has been "orphaned" -- most likely because the prospective
+        ///  inviter has deleted his account.
+        /// </summary>
+        Orphaned
+    }
+}
