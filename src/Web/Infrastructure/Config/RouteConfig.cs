323e3b22feabd010a976dec7daa3e1e139a6f850
James McKay <code@jamesmckay.net>
2013-05-08 18:07:23 +0100

    Enforce namespace for default routes.
    This allows us to have controllers called "Home" in each area.

--- src/Web/Infrastructure/Config/RouteConfig.cs
+++ src/Web/Infrastructure/Config/RouteConfig.cs
@@ -16,7 +16,8 @@ namespace Dolstagis.Web.Infrastructure.Config
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                namespaces: new[] { "Dolstagis.Web.Controllers" }
             );
         }
     }
