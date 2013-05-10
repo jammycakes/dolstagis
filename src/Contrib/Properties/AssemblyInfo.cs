a73ee573ccf10a5fdece184441526b74cf9facf5
James McKay <code@jamesmckay.net>
2013-05-10 18:52:48 +0100

    Rename Batteries to Contrib.
    --HG--
    rename : src/Batteries/Accounts/AccountSettings.cs => src/Contrib/Accounts/AccountSettings.cs
    rename : src/Batteries/Accounts/AccountsNinjectModule.cs => src/Contrib/Accounts/AccountsNinjectModule.cs
    rename : src/Batteries/Accounts/IAccountSettings.cs => src/Contrib/Accounts/IAccountSettings.cs
    rename : src/Batteries/Accounts/Invitation.cs => src/Contrib/Accounts/Invitation.cs
    rename : src/Batteries/Accounts/InvitationManager.cs => src/Contrib/Accounts/InvitationManager.cs
    rename : src/Batteries/Accounts/InvitationState.cs => src/Contrib/Accounts/InvitationState.cs
    rename : src/Batteries/Accounts/Mappings/InvitationMap.cs => src/Contrib/Accounts/Mappings/InvitationMap.cs
    rename : src/Batteries/Accounts/Mappings/UserMap.cs => src/Contrib/Accounts/Mappings/UserMap.cs
    rename : src/Batteries/Accounts/Mappings/UserSessionMap.cs => src/Contrib/Accounts/Mappings/UserSessionMap.cs
    rename : src/Batteries/Accounts/Mappings/UserTokenMap.cs => src/Contrib/Accounts/Mappings/UserTokenMap.cs
    rename : src/Batteries/Accounts/Passwords/AggregatePasswordProvider.cs => src/Contrib/Accounts/Passwords/AggregatePasswordProvider.cs
    rename : src/Batteries/Accounts/Passwords/BCrypt/BCryptPasswordProvider.cs => src/Contrib/Accounts/Passwords/BCrypt/BCryptPasswordProvider.cs
    rename : src/Batteries/Accounts/Passwords/BCrypt/BCryptSettings.cs => src/Contrib/Accounts/Passwords/BCrypt/BCryptSettings.cs
    rename : src/Batteries/Accounts/Passwords/BCrypt/IBCryptSettings.cs => src/Contrib/Accounts/Passwords/BCrypt/IBCryptSettings.cs
    rename : src/Batteries/Accounts/Passwords/IPasswordProvider.cs => src/Contrib/Accounts/Passwords/IPasswordProvider.cs
    rename : src/Batteries/Accounts/Passwords/PasswordResult.cs => src/Contrib/Accounts/Passwords/PasswordResult.cs
    rename : src/Batteries/Accounts/Passwords/PlainText/PlainTextPasswordProvider.cs => src/Contrib/Accounts/Passwords/PlainText/PlainTextPasswordProvider.cs
    rename : src/Batteries/Accounts/Passwords/Sha512/Sha512PasswordProvider.cs => src/Contrib/Accounts/Passwords/Sha512/Sha512PasswordProvider.cs
    rename : src/Batteries/Accounts/User.cs => src/Contrib/Accounts/User.cs
    rename : src/Batteries/Accounts/UserManager.cs => src/Contrib/Accounts/UserManager.cs
    rename : src/Batteries/Accounts/UserSession.cs => src/Contrib/Accounts/UserSession.cs
    rename : src/Batteries/Accounts/UserToken.cs => src/Contrib/Accounts/UserToken.cs
    rename : src/Batteries/Dolstagis.Batteries.csproj => src/Contrib/Dolstagis.Contrib.csproj
    rename : src/Batteries/LICENSE.txt => src/Contrib/LICENSE.txt
    rename : src/Batteries/Properties/AssemblyInfo.cs => src/Contrib/Properties/AssemblyInfo.cs
    rename : src/Batteries/packages.config => src/Contrib/packages.config

--- src/Contrib/Properties/AssemblyInfo.cs
+++ src/Contrib/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Dolstagis.Batteries")]
+[assembly: AssemblyTitle("Dolstagis.Contrib")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Dolstagis Solutions")]
-[assembly: AssemblyProduct("Batteries")]
+[assembly: AssemblyProduct("Contrib")]
 [assembly: AssemblyCopyright("Copyright © Dolstagis Solutions 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
