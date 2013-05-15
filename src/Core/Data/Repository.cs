625342a0f0679162177a2b45634c0fadd0230f10
James McKay <code@jamesmckay.net>
2013-05-15 08:08:29 +0100

    Add some key methods to the repository base class.

--- src/Core/Data/Repository.cs
+++ src/Core/Data/Repository.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using NHibernate;
+using NHibernate.Linq;
 
 namespace Dolstagis.Core.Data
 {
@@ -44,6 +45,38 @@ namespace Dolstagis.Core.Data
                 this.Session.Flush();
                 this.Session.Dispose();
             }
+        }
+
+        public TModel Get(object id)
+        {
+            return Session.Get<TModel>(id);
+        }
+
+        public IQueryable<TModel> Query()
+        {
+            return Session.Query<TModel>();
+        }
+
+        public virtual TModel CreateTransient()
+        {
+            return new TModel();
+        }
+
+        public TModel CreatePersistent()
+        {
+            var result = CreateTransient();
+            Session.Persist(result);
+            return result;
+        }
+
+        public void Persist(TModel obj)
+        {
+            Session.Persist(obj);
+        }
+
+        public void Flush()
+        {
+            Session.Flush();
         }
     }
 }
