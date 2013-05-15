880689e52fe51feab5547c41b41073422421fe68
James McKay <code@jamesmckay.net>
2013-05-15 17:50:55 +0100

    Manager should implement IDisposable

--- src/Core/Manager.cs
+++ src/Core/Manager.cs
@@ -1,17 +1,34 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using Dolstagis.Core.Data;
 using NHibernate;
 using NHibernate.Linq;
 
 namespace Dolstagis.Core
 {
-    public class Manager<TModel> where TModel: new()
+    public class Manager<TModel> : IDisposable where TModel: new()
     {
         public IRepository<TModel> Repository { get; private set; }
 
         public Manager(IRepository<TModel> repository)
         {
             this.Repository = repository;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (Repository is IDisposable) ((IDisposable)Repository).Dispose();
+        }
+
+        ~Manager()
+        {
+            Dispose(false);
         }
     }
 
