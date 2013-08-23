8eeeb6353392f9f6bf4952ed8929bb4c7e353cb1
James McKay <code@jamesmckay.net>
2013-08-23 07:36:00 +0100

    Fix migrate.bat for new paths.

--- migrate.bat
+++ migrate.bat
@@ -1,3 +1,3 @@
-src\packages\FluentMigrator.Tools.1.0.5.0\tools\AnyCPU\40\Migrate.exe ^
+src\packages\FluentMigrator.Tools.1.1.1.0\tools\AnyCPU\40\Migrate.exe ^
     --provider sqlserver2012 ^
-    --a src\DataMigrations\bin\debug\Dolstagis.DataMigrations.dll
+    --a src\Dolstagis.DataMigrations\bin\debug\Dolstagis.DataMigrations.dll
