80d6d83e8d421b37df11437baa3452af8bb5c2fe
James McKay <code@jamesmckay.net>
2013-09-22 23:37:37 +0100

    RouteAttribute and RegexRouteAttribute for route definitions.

---
+++ src/Dolstagis.Web/Routing/RouteAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dolstagis.Web.Routing
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true, Inherited = false)]
+    public class RouteAttribute : Attribute
+    {
+        public string Route { get; private set; }
+
+        public string Method { get; private set; }
+
+        public RouteAttribute(string route, string method = null)
+        {
+            this.Route = route;
+            this.Method = method;
+        }
+    }
+}
