954512eb383ab50b41cf1e08efa0387e747de13f
James McKay <code@jamesmckay.net>
2013-09-22 10:00:02 +0100

    Add basic implementation of IResponse.

--- src/Dolstagis.Web.Aspnet/Response.cs
+++ src/Dolstagis.Web.Aspnet/Response.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web;
@@ -14,6 +15,28 @@ namespace Dolstagis.Web.Aspnet
         public Response(HttpResponseBase httpResponse)
         {
             this.httpResponse = httpResponse;
+        }
+
+        public void AddHeader(string name, string value)
+        {
+            httpResponse.AppendHeader(name, value);
+        }
+
+        public System.IO.Stream ResponseStream
+        {
+            get { return httpResponse.OutputStream; }
+        }
+
+        public int StatusCode
+        {
+            get { return this.httpResponse.StatusCode; }
+            set { this.httpResponse.StatusCode = value; }
+        }
+
+        public string StatusDescription
+        {
+            get { return this.httpResponse.StatusDescription; }
+            set { this.httpResponse.StatusDescription = value; }
         }
     }
 }
--- src/Dolstagis.Web/IResponse.cs
+++ src/Dolstagis.Web/IResponse.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,12 @@ namespace Dolstagis.Web
 {
     public interface IResponse
     {
+        void AddHeader(string name, string value);
+
+        Stream ResponseStream { get; }
+
+        int StatusCode { get; set; }
+
+        string StatusDescription { get; set; }
     }
 }
