5ca2604618030d031fe598bd78130561673ad4dd
James McKay <code@jamesmckay.net>
2013-09-22 09:41:44 +0100

    Create application lazily
    This lets us use a subclass if necessary.

--- src/Dolstagis.Web.Aspnet/DolstagisHttpModule.cs
+++ src/Dolstagis.Web.Aspnet/DolstagisHttpModule.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Web;
 
@@ -9,14 +10,23 @@ namespace Dolstagis.Web.Aspnet
 {
     public class DolstagisHttpModule : IHttpModule
     {
-        private static Application application = new Application();
+        public static Func<Application> ApplicationFactory { get; set; }
+
+        private static Lazy<Application> application
+            = new Lazy<Application>(CreateApplication, LazyThreadSafetyMode.ExecutionAndPublication);
+
         private static int refCount = 0;
+
+        private static Application CreateApplication()
+        {
+            return ApplicationFactory != null ? ApplicationFactory() : new Application();
+        }
 
         public void Dispose()
         {
             lock (application) {
-                if (--refCount == 0) {
-                    application.Dispose();
+                if (--refCount == 0 && application.IsValueCreated) {
+                    application.Value.Dispose();
                 }
             }
         }
@@ -24,7 +34,7 @@ namespace Dolstagis.Web.Aspnet
         public void Init(HttpApplication context)
         {
             refCount++;
-            context.BeginRequest += (s, e) => context.Context.RemapHandler(new DolstagisHttpHandler(application));
+            context.BeginRequest += (s, e) => context.Context.RemapHandler(new DolstagisHttpHandler(application.Value));
         }
     }
 }
