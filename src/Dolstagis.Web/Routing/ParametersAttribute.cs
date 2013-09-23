f98ae3236d56294e71820d774500071833affb41
James McKay <code@jamesmckay.net>
2013-09-23 21:45:16 +0100

    ParametersAttribute
    Yes, I know it's the same as RouteAttribute. For now...this might change...

---
+++ src/Dolstagis.Web/Routing/ParametersAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dolstagis.Web.Routing
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public class ParametersAttribute : Attribute
+    {
+        public string Path { get; private set; }
+
+        public IList<string> Components { get; private set; }
+
+        public ParametersAttribute(string path)
+        {
+            this.Path = path;
+            this.Components = path.Split(new char[] { '/' }, StringSplitOptions.RemoveEmptyEntries)
+                .ToList().AsReadOnly();
+        }
+    }
+}
