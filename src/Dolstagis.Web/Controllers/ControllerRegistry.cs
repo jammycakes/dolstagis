05b6245efb44ef857dc806ebbfc2d60d8e048733
James McKay <code@jamesmckay.net>
2013-09-23 17:22:39 +0100

    ControllerRegistry

---
+++ src/Dolstagis.Web/Controllers/ControllerRegistry.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dolstagis.Web.Controllers
+{
+    public class ControllerRegistry
+    {
+        private Func<Module[]> modules;
+
+        public ControllerRegistry(Func<Module[]> modules)
+        {
+            this.modules = modules;
+        }
+    }
+}
