bb6c8e4bd44c9204e2a5b252f2c5ae5656490b02
James McKay <code@jamesmckay.net>
2013-09-24 17:50:15 +0100

    HandlerRegistration class.

---
+++ src/Dolstagis.Web/Handlers/HandlerRegistration.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dolstagis.Web.Handlers
+{
+    public class HandlerRegistration
+    {
+        private IDictionary<string, HandlerRegistration> children
+            = new Dictionary<string, HandlerRegistration>(StringComparer.OrdinalIgnoreCase);
+
+        public HandlerDefinition Definition { get; private set; }
+
+        public HandlerRegistration Parent { get; private set; }
+
+        public HandlerRegistration GetChild(string name)
+        {
+            HandlerRegistration result;
+            return children.TryGetValue(name, out result) ? result : null;
+        }
+
+        public HandlerRegistration(HandlerDefinition definition, HandlerRegistration parent, string name)
+        {
+            this.Definition = definition;
+            this.Parent = parent;
+            this.Parent.children[name] = this;
+        }
+    }
+}
