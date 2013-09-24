284a0817e1e270855e8b6950c9441bdc32dba605
James McKay <code@jamesmckay.net>
2013-09-24 22:22:51 +0100

    ActionLocator stub.

---
+++ src/Dolstagis.Web/ActionLocator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dolstagis.Web.Handlers;
+
+namespace Dolstagis.Web
+{
+    public class ActionLocator
+    {
+        private HandlerRegistry handlers;
+        private Func<IAction> getInvoker;
+
+        public ActionLocator(HandlerRegistry handlers, Func<IAction> getInvoker)
+        {
+            this.handlers = handlers;
+            this.getInvoker = getInvoker;
+        }
+
+        public IAction GetAction(IRequest request)
+        {
+            return null;
+        }
+    }
+}
