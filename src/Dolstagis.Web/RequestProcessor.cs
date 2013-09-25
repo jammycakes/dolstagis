a2f130204fdb414f64e785cf29497c0876eb040a
James McKay <code@jamesmckay.net>
2013-09-25 08:00:58 +0100

    Process request.

--- src/Dolstagis.Web/RequestProcessor.cs
+++ src/Dolstagis.Web/RequestProcessor.cs
@@ -9,9 +9,25 @@ namespace Dolstagis.Web
 {
     public class RequestProcessor : IRequestProcessor
     {
+        private IActionLocator actionLocator;
+        private Func<IViewBuilder> getViewBuilder;
+
+        public RequestProcessor(IActionLocator actionLocator, Func<IViewBuilder> getViewBuilder)
+        {
+            this.actionLocator = actionLocator;
+            this.getViewBuilder = getViewBuilder;
+        }
+
         public void Process(IRequestContext context)
         {
-
+            var action = actionLocator.GetAction(context.Request);
+            if (action != null) {
+                var model = action.Invoke();
+                IView view = (model as IView) ?? getViewBuilder().CreateView(model);
+                if (view != null) {
+                    view.Render(context.Response);
+                }
+            }
         }
     }
 }
