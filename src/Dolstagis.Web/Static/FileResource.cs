46ebbc93b2fbb82af6ca222dc4ecec875c4887bf
James McKay <code@jamesmckay.net>
2013-09-26 23:59:37 +0100

    Static file resource locator

---
+++ src/Dolstagis.Web/Static/FileResource.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dolstagis.Web.Static
+{
+    public class FileResource : IResource
+    {
+        private FileInfo _file;
+
+        public FileResource(FileInfo file)
+        {
+            this._file = file;
+        }
+
+        public DateTime DateModified
+        {
+            get { return _file.LastWriteTimeUtc; }
+        }
+
+        public long Length
+        {
+            get { return _file.Length; }
+        }
+
+        public string Name
+        {
+            get { return _file.Name; }
+        }
+
+        public System.IO.Stream Open()
+        {
+            return new FileStream(_file.FullName, FileMode.Open, FileAccess.Read);
+        }
+    }
+}
---
+++ src/Dolstagis.Web/Static/FileResourceLocator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dolstagis.Web.Static
+{
+    public class FileResourceLocator : IResourceLocator
+    {
+        private string _root;
+
+        private static string NormPath(string path)
+        {
+            return Path.GetFullPath(path);
+        }
+
+        public FileResourceLocator(string root)
+        {
+            _root = NormPath(root);
+            if (!_root.EndsWith(Path.DirectorySeparatorChar.ToString())) {
+                _root += Path.DirectorySeparatorChar;
+            }
+        }
+
+        public IResource GetResource(string path)
+        {
+            var abspath = NormPath(Path.Combine(_root, path));
+            if (!abspath.StartsWith(_root, StringComparison.OrdinalIgnoreCase)) return null;
+            var file = new FileInfo(abspath);
+            if (file.Exists) {
+                return new FileResource(file);
+            }
+            else {
+                return null;
+            }
+        }
+    }
+}
